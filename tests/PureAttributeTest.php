4c9f25352d2f18f6f7fb970b3b4e39eca15c49cd
Carlos Granados <barel.barelon@gmail.com>
2024-02-25 16:01:09 +0100

    Add Pure and Impure attributes

---
+++ tests/PureAttributeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace test\PhpStaticAnalysis\PsalmPlugin;
+
+class PureAttributeTest extends BaseAttributeTestCase
+{
+    public function testMethodPureAttribute(): void
+    {
+        $errors = $this->analyzeTestFile('/data/Pure/MethodPureAttribute.php');
+        $this->assertCount(0, $errors);
+    }
+
+    public function testFunctionPureAttribute(): void
+    {
+        $errors = $this->analyzeTestFile('/data/Pure/FunctionPureAttribute.php');
+        $this->assertCount(0, $errors);
+    }
+
+    public function testInvalidMethodPureAttribute(): void
+    {
+        $errors = $this->analyzeTestFile('/data/Pure/InvalidMethodPureAttribute.php');
+
+        $expectedErrors = [
+            'Attribute Pure is not repeatable' => 15,
+            'Attribute Pure cannot be used on a property' => 11,
+        ];
+
+        $this->checkExpectedErrors($errors, $expectedErrors);
+    }
+}
